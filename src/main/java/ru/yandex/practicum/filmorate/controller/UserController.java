d6a23691cbab8ad63436fae76a865d71f37f94ba
mesavage <fatal.k@yandex.ru>
2023-06-01 21:09:00 +0300

    added user and movie models and controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exeptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    Map<Integer, User> users = new HashMap<>();
+    Integer id = 0;
+
+    private void idGenerator(User user) {
+        ++id;
+        user.setId(id);
+    }
+
+    @PostMapping
+    public User addUser(@Valid @RequestBody User user) {
+        idGenerator(user);
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        if(users.get(user.getId()) == null) {
+            throw new ValidationException("UnknownUser");
+        }
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+    @GetMapping
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
