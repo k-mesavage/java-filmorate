d206822451060f474fa5ed1b4f079385cc2da581
mesavage <fatal.k@yandex.ru>
2023-06-01 13:20:50 +0300

    First Commit

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,11 +3,13 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class FilmorateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
-
+ArrayList<String> a = new ArrayList<>();
 }
