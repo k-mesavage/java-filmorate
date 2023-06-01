89c61c561e734a3ce6e57e3139a771022b08e451
mesavage <fatal.k@yandex.ru>
2023-06-01 21:44:53 +0300

    added user and movie models and controllers

--- src/main/java/ru/yandex/practicum/filmorate/exeptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeptions/ValidationException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.exeptions;
 
 
 public class ValidationException extends javax.validation.ValidationException {
- public ValidationException (String message) {
+ public ValidationException(String message) {
      super(message);
  }
 }
